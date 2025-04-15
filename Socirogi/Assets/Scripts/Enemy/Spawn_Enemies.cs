dfc37e501b0ed2bcc7e1f67b5e13334cd31ccdfc
StevenSeagull1 <stevenhoekstra2005@gmail.com>
2025-04-15 02:37:56 +0200

    bezig geweest met de muren die spawnen wanneer de speler er is

--- Socirogi/Assets/Scripts/Enemy/Spawn_Enemies.cs
+++ Socirogi/Assets/Scripts/Enemy/Spawn_Enemies.cs
@@ -19,6 +19,8 @@ public class Spawn_Enemies : MonoBehaviour
     private bool isPlayerNearby = false;
     private bool enemiesSpawned = false;
 
+    public GameObject Wall;
+
     //wanneer de player colide met de trigger collsion spawn de enemies in
     void OnTriggerEnter(Collider other)
     {
@@ -41,13 +43,54 @@ public class Spawn_Enemies : MonoBehaviour
                 enemy.spawnedInstance = Instantiate(enemy.enemyPrefab, enemy.spawnPoint.position, enemy.spawnPoint.rotation);
             }
         }
-
+        Block_Room();
         enemiesSpawned = true;
     }
 
+    public void Block_Room()
+    {
+        Instantiate(Wall, new Vector3(0, 19, 2), Quaternion.identity);
+
+    }
+
+    public void UnBlock_Room()
+    {
+        //pak alle objecten
+        GameObject[] alleObjecten = FindObjectsOfType<GameObject>();
+
+        //ga door elk object heen
+        foreach (GameObject obj in alleObjecten)
+
+        {
+            //als het object naam deze naam heeft wordt het verwijderd.
+            if (obj.name == "de_muur(Clone)")
+            {
+                Destroy(obj);
+            }
+        }
 
 
+    }
 
+    void Update()
+    {
+        if (enemiesSpawned)
+        {
+            bool allEnemiesDead = true;
+            foreach (var enemy in enemies)
+            {
+                if (enemy.spawnedInstance != null)
+                {
+                    allEnemiesDead = false;
+                    break;
+                }
+            }
 
-    
+            if (allEnemiesDead)
+            {
+                UnBlock_Room();
+                enemiesSpawned = false;
+            }
+        }
+    }
 }
