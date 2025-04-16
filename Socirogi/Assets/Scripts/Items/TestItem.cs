77749b694cd86019b06a515980908c1da055e5c5
RaynisonTD <raynison.daal@student.hu.nl>
2025-04-16 16:29:45 +0200

    inventory scripable objects
    items zijn nu scriptable objects die in de folders aangemaakt kunnen worden
    
    1: Als je een item wilt maken: right click in de folder > create> onderaan staat een tab "inventory" en dan kan je een nieuw test item maken als, als je hem maakt kan je het een naam max aantal geven en een foto geven
    2: in het geval dat je een nieuw item script wil maken  kan je in de Items map in die in de scripts map staat een nieuwe item maken die kan je dan bijvoorbeeld potion noemen

---
+++ Socirogi/Assets/Scripts/Items/TestItem.cs
@@ -0,0 +1,11 @@
+using UI.Inventory_System;
+using UnityEngine;
+
+namespace Items
+{
+    [CreateAssetMenu(menuName = "Inventory/Items/Test", fileName = "New Test Item")]
+    public class TestItem : Item
+    {
+    
+    }
+}
--- Socirogi/Assets/Scripts/UI/Inventory System/Item.cs
+++ Socirogi/Assets/Scripts/UI/Inventory System/Item.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 namespace UI.Inventory_System
 {
+   
     public abstract class Item : ScriptableObject
     {
-        public abstract string GiveName();
+        [HideInInspector]
+        public string itemName;
+        
+        [HideInInspector]
+        public Sprite itemImage;
+        
+        [HideInInspector]
+        public int maxQuantity;
 
-        public virtual int MaxQuantity()
-        {
-            return 1;
-        }
-
-        public virtual Sprite GetItemIcon()
-        {
-             return Resources.Load<Sprite>("UI/Images/Item Images/No Item Image Icon");
-        }
+        public virtual string GiveName() => itemName;
+        public virtual Sprite GetItemImage() => itemImage;
     }
 }
-
